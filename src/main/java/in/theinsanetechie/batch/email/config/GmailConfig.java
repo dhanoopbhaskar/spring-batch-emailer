66f7a971a6dcb68c57c1606958a1cab733e1c418
Dhanoop Bhaskar <dhanoopbhaskar@gmail.com>
2020-10-21 11:26:39 +0530

    deleted debug code

--- src/main/java/in/theinsanetechie/batch/email/config/GmailConfig.java
+++ src/main/java/in/theinsanetechie/batch/email/config/GmailConfig.java
@@ -5,21 +5,16 @@ import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInsta
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
-import com.google.api.services.gmail.model.Label;
-import com.google.api.services.gmail.model.ListLabelsResponse;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 
@@ -60,23 +55,4 @@ public class GmailConfig {
 		return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
 	}
 
-	public static void main(String... args) throws IOException, GeneralSecurityException {
-		// Build a new authorized API client service.
-		final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-		Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-				.setApplicationName(APPLICATION_NAME).build();
-
-		// Print the labels in the user's account.
-		String user = "me";
-		ListLabelsResponse listResponse = service.users().labels().list(user).execute();
-		List<Label> labels = listResponse.getLabels();
-		if (labels.isEmpty()) {
-			System.out.println("No labels found.");
-		} else {
-			System.out.println("Labels:");
-			for (Label label : labels) {
-				System.out.printf("- %s\n", label.getName());
-			}
-		}
-	}
 }
