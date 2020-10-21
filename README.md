9721d16a3d15ac884c6167cb4dce49cffa7650d5
Dhanoop Bhaskar <dhanoopbhaskar@gmail.com>
2020-10-21 11:21:25 +0530

    update - using Gmail Java API

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# spring-batch-emailer
+# spring-batch-emailer - NEW VERSION
+Spring Batch Emailer
+
+1. Enable [Enable the Gmail API.](https://developers.google.com/gmail/api/quickstart/java)
+
+2. Download Client Configuration as `credentials.json` and place it in the directory `src/main/resources`.
+
+3. Check the username in `application.properties` file.
+
+		spring.mail.username
+
+4. Check the input file path in `application.properties` file and ensure the presence of input file in the specified path.
+
+		input.filepath
+
+5. On first run, it will ask us to visit a URL and authorize the application. On successful authorization, it will create a file, `StoredCredential` in the directory, `tokens`. Kindly note that, if you delete the file, it will again ask for authorization.
+
+
+# spring-batch-emailer - OLD VERSION
 Spring Batch Emailer
 
 1. Check the login credentials in `application.properties` file.
--- src/main/java/in/theinsanetechie/batch/email/job/EmailService.java
+++ src/main/java/in/theinsanetechie/batch/email/job/EmailService.java
@@ -1,11 +1,29 @@
 package in.theinsanetechie.batch.email.job;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Properties;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.util.Base64;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Message;
+
+import in.theinsanetechie.batch.email.config.GmailConfig;
 
 /**
  * 
@@ -19,7 +37,7 @@ public class EmailService {
 	private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
 
 	@Autowired
-	private JavaMailSender mailSender;
+	private Environment environment;
 
 	/**
 	 * 
@@ -28,13 +46,49 @@ public class EmailService {
 	 * @param body
 	 * 
 	 *                Compose and send email
+	 * @throws MessagingException
+	 * @throws AddressException
+	 * @throws IOException
+	 * @throws GeneralSecurityException
 	 */
-	public void sendMail(String to, String subject, String body) {
-		SimpleMailMessage message = new SimpleMailMessage();
-		message.setTo(to);
-		message.setSubject(subject);
-		message.setText(body);
-		logger.info(">>>>>>>> " + to + "|" + subject + "|" + body);
-		mailSender.send(message);
+	public void sendMail(String to, String subject, String body)
+			throws AddressException, MessagingException, IOException, GeneralSecurityException {
+
+		String userName = environment.getProperty("spring.mail.username", "dhanoopbhaskar@gmail.com");
+
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+
+		/**
+		 * Create a MimeMessage using the parameters provided.
+		 */
+		MimeMessage email = new MimeMessage(session);
+		email.setFrom(new InternetAddress(userName));
+		email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
+		email.setSubject(subject);
+		email.setText(body);
+		logger.debug(">>>>>>>> " + to + "|" + subject + "|" + body);
+
+		/**
+		 * Create a message from an email.
+		 */
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		email.writeTo(buffer);
+		byte[] bytes = buffer.toByteArray();
+		String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
+		Message message = new Message();
+		message.setRaw(encodedEmail);
+
+		/**
+		 * Send an email from the user's mailbox to its recipient.
+		 */
+		final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+		Gmail service = new Gmail.Builder(HTTP_TRANSPORT, GmailConfig.JSON_FACTORY,
+				GmailConfig.getCredentials(HTTP_TRANSPORT)).setApplicationName(GmailConfig.APPLICATION_NAME).build();
+
+		message = service.users().messages().send(userName, message).execute();
+
+		logger.debug("Message id: " + message.getId());
+		logger.debug(message.toPrettyString());
 	}
 }
